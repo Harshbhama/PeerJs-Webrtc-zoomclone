4127864b12e4d6a9c57348af1cdf2b789362b726
Harsh Bhama <harshbhama96@gmail.com>
2024-01-17 21:51:47 +0530

    add new server for io

---
+++ new_server.js
@@ -0,0 +1,27 @@
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"]
+    }
+})
+io.on("connection", (socket) => {
+    console.log('A user is connected')
+    socket.on("message", (message, roomName) => {
+        if(roomName.length){
+            io.to(roomName).emit("message" ,message, roomName)
+        }else{
+            io.emit("message", message)
+        }
+        
+        console.log(message, roomName);
+    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected")
+    })
+    socket.on('joinedRoom', (roomName) => {
+        console.log("Joined room: ", roomName)
+        socket.join(roomName)
+    })
+    // console.log("Current room", socket.rooms)
+})
+console.log("Hello")
